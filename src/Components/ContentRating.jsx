5312b84feac0c4ec7717b42d55022983cf70ec8e
Charles <charlesantonmalazarte25@outlook.com>
2025-09-19 16:35:31 -0300

    Adding content to rate and buttons to click

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -4,12 +4,41 @@ import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
-    super();
+    super(); 
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      handleLike:() => {
+        this.setState((prevState)=>({
+          likes: prevState.likes + 1
+        }));
+      },
+
+      handleDislike:()=> {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1
+        }))
+      }
+    };
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className='content-rating'>
+      <p>
+        Latest <i>Unreal Engine 5</i> has been revolutionary in Gaming Industries.
+        With some popular games that has been using this engine; <b>Tekken 8, Black Myth:
+        Wukong, and Layers of Fear</b>. Do you agree? </p>
+     </div>
+
+     <div className='rating-buttons'>
+      <button className='like-button' onClick={this.state.handleLike}>
+        Like ({this.state.likes})
+      </button>
+      <button className='dislike-button'onClick={this.state.handleDislike}>
+        Dislike ({this.state.dislikes})
+      </button>
+     </div>
      </>
     );
   }
